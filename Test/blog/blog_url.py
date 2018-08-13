c7d2ad4e1f11387821e2d16e87c29de0552deff6
Beanzheng <Bean_zheng@163.com>
2018-08-13 17:10:34 +0800

    models简单列子

--- Test/blog/blog_url.py
+++ Test/blog/blog_url.py
@@ -14,4 +14,15 @@ urlpatterns = [
     url(r'book/(?:page-(?P<pagenumber>\d+)/)?$',views.findNumber),
     url(r'yourname',views.extraparam,{"name":"郑高翔"}),
     url(r'mayiknowyouname/$',views.revParse,name="askname"),
+
+    url(r'/v10_1',views.v10_1),
+    url(r'/v10_2',views.v10_2),
+    url(r'/v11_hello',views.v11, name="hello"),
+    url(r'/getRsq/',views.getReqContent),
+    url(r'/blog_get',views.blog_get),
+    url(r'/blog_post',views.blog_post),
+    url(r'/render_test',views.render_test),
+    url(r'render2_test',views.render2_test),
+    url(r'render3_test',views.render3_test),
+    url('/get404',views.get404),
 ]
--- Test/blog/views.py
+++ Test/blog/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render,render_to_response
+from django.http import HttpResponse,HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.template import loader
+from django.views import defaults
 # Create your views here.
 
 def do_normal(request):
@@ -19,3 +21,52 @@ def extraparam(request,name):
 
 def revParse(request):
     return HttpResponse("Request URl is {0}".format(reverse("askname")))
+
+def v10_1(request):
+    return HttpResponseRedirect("/v11")
+
+def v10_2(request):
+    return HttpResponseRedirect(reverse("hello"))
+
+def v11(request):
+    return HttpResponse("你终于访问到我了!")
+
+def getReqContent(request):
+    rsp = ""
+    for k,v in request.GET.items():
+        rsp += k+"----"+v
+        rsp += ","
+
+    return HttpResponse("This is Request: {0}".format(rsp))
+
+def blog_get(request):
+    return render_to_response("for_post.html")
+
+def blog_post(request):
+    rsq =""
+    for k,v in request.POST.items():
+        rsq +=k +"----"+v
+        rsq += ","
+
+    return HttpResponse("Message:{0}".format(rsq))
+
+def render_test(request):
+    rsp = render(request,"renderTest.html")
+    return rsp
+
+def render2_test(request):
+    c=dict()
+    c["name"]="郑高翔"
+    rsp = render(request,"renderTest2.html",context=c)
+    return rsp
+
+def render3_test(request):
+    t = loader.get_template("renderTest2.html")
+
+    rsp = t.render({"name":"zgx"})
+
+    return HttpResponse(rsp)
+
+def get404(request):
+
+    return defaults.page_not_found(request,template_name="renderTest.html")
